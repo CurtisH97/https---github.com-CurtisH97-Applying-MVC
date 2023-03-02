2b00ae406d5e4258c140a18953314b5947927b78
Curtis <crhill2@mail.fhsu.edu>
2023-03-01 21:56:11 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,103 @@
+<?php
+require_once('database.php');
+
+// Get category ID
+if (!isset($category_id)) {
+    $category_id = filter_input(INPUT_GET, 'category_id', 
+            FILTER_VALIDATE_INT);
+    if ($category_id == NULL || $category_id == FALSE) {
+        $category_id = 1;
+    }
+}
+// Get name for selected category
+$queryCategory = 'SELECT * FROM categories
+                  WHERE categoryID = :category_id';
+$statement1 = $db->prepare($queryCategory);
+$statement1->bindValue(':category_id', $category_id);
+$statement1->execute();
+$category = $statement1->fetch();
+$category_name = $category['categoryName'];
+$statement1->closeCursor();
+
+
+// Get all categories
+$query = 'SELECT * FROM categories
+                       ORDER BY categoryID';
+$statement = $db->prepare($query);
+$statement->execute();
+$categories = $statement->fetchAll();
+$statement->closeCursor();
+
+// Get products for selected category
+$queryProducts = 'SELECT * FROM todoitems
+                  WHERE categoryID = :category_id
+                  ORDER BY ItemNum';
+$statement3 = $db->prepare($queryProducts);
+$statement3->bindValue(':category_id', $category_id);
+$statement3->execute();
+$products = $statement3->fetchAll();
+$statement3->closeCursor();
+?>
+<!DOCTYPE html>
+<html>
+
+<!-- the head section -->
+<head>
+    <title>ToDoList</title>
+    <link rel="stylesheet" type="text/css" href="main.css" />
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>ToDoList Manager</h1></header>
+<main>
+    <h1>ToDoList</h1>
+
+    <aside>
+        <!-- display a list of categories -->
+        <h2>Categories</h2>
+        <nav>
+        <ul>
+            <?php foreach ($categories as $category) : ?>
+            <li><a href=".?category_id=<?php echo $category['categoryID']; ?>">
+                    <?php echo $category['categoryName']; ?>
+                </a>
+            </li>
+            <?php endforeach; ?>
+        </ul>
+        </nav>          
+    </aside>
+
+    <section>
+        <!-- display a table of products -->
+        <h2><?php echo $category_name; ?></h2>
+        <table>
+            <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th>&nbsp;</th>
+            </tr>
+
+            <?php foreach ($products as $product) : ?>
+            <tr>
+                <td><?php echo $product['Title']; ?></td>
+                <td><?php echo $product['Description']; ?></td>
+                <td><form action="delete_product.php" method="post">
+                <input type="hidden" name="itemNum"
+                           value="<?php echo $product['ItemNum']; ?>">
+                    <input type="hidden" name="category_id"
+                           value="<?php echo $product['categoryID']; ?>">
+                    <input type="submit" value="Delete">
+                </form></td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+        <p><a href="add_product_form.php">Add Product</a></p>
+        <p><a href="category_list.php">List Categories</a></p>        
+    </section>
+</main>
+<footer>
+    <p>&copy; <?php echo date("Y"); ?> ToDoList.</p>
+</footer>
+</body>
+</html>
